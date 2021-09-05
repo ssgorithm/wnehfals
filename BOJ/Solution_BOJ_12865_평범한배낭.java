3f7e53860d28c3cf7b6e779f64f506c2319e4010
wnehfals <33659654+wnehfals@users.noreply.github.com>
2021-09-05 15:39:12 +0900

    2021_09_05_다이나믹프로그래밍_골드5

---
+++ BOJ/Solution_BOJ_12865_평범한배낭.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution_BOJ_12865_평범한배낭 {
+
+	static int N,W;
+	static int knapsack[][];
+	static int weight[];
+	static int value[];
+	static int max = Integer.MIN_VALUE;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		W = Integer.parseInt(st.nextToken());
+		
+		knapsack = new int [N+1][W+1];
+		weight = new int [N+1];
+		value = new int [N+1];
+		
+		for(int i=1; i<=N; i++) {
+			st = new StringTokenizer(br.readLine());
+			weight[i] = Integer.parseInt(st.nextToken());
+			value[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=W; j++) {
+				if(weight[i]<=j) {
+					knapsack[i][j] = Math.max(value[i]+knapsack[i-1][j-weight[i]],knapsack[i-1][j]);
+				}
+				else knapsack[i][j] = knapsack[i-1][j];
+				max = Math.max(max, knapsack[i][j]);
+			}
+		}
+		System.out.println(max);
+		
+	}
+
+}
